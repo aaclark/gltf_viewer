6d9b26ee42b26ba0a5291cd42d0970cb247f96f5
Alain Clark <aac.clark@gmail.com>
2025-03-10 15:39:14 +0100

    escape strings because bash

--- build.sh
+++ build.sh
@@ -7,17 +7,19 @@ then
 fi
 
 # Generate build script
-cd $MODEL_VIEWER_ROOT && \
+cd "$MODEL_VIEWER_ROOT" && \
 if [ ! -d build ]; then
     mkdir build
 fi
 cd build && \
-cmake ../ -DCMAKE_INSTALL_PREFIX=$MODEL_VIEWER_ROOT && \
+cmake ../ -DCMAKE_INSTALL_PREFIX="$MODEL_VIEWER_ROOT" && \
+
+cd ../ && \
 
 # Build and install the program
 make -j4 && \
 make install && \
 
 # Run the program
-cd ../bin && \
+cd ../build && \
 ./model_viewer
